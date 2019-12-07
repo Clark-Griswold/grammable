c2fda6568d00b5793b8302afe7a02d8195e82378
Clark-Griswold <dustindavis1@gmail.com>
2019-12-07 21:14:14 +0000

    creating a gram

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,4 +1,5 @@
 class GramsController < ApplicationController
+  
   def new
     @gram = Gram.new
   end
@@ -7,6 +8,15 @@ class GramsController < ApplicationController
   def index
   end
 
+  def create
+    @gram = Gram.create(gram_params)
+    redirect_to root_path
+  end
 
+  private
+
+  def gram_params
+    params.require(:gram).permit(:message)
+  end
 
 end
